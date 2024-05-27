cdcbaded5d38c5dda21af7a6cd234a3a500eebbe
Jorge Torres <fervirlacrer@gmail.com>
2024-05-27 21:18:52 +0000

    Adding the endpoints

--- index.js
+++ index.js
@@ -20,11 +20,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/whoami", (req, res)=> {
+  return res.json ({
+    ipaddress:req.ip,
+    language:req.headers["accept-language"],
+    software:req.headers["user-agent"]
+  })
+  console.log(req.ip)
+  console.log(req.headers["accept-language"])
+  console.log(req.headers["user-agent"])
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3020, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
